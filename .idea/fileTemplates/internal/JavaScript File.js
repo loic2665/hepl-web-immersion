c229505a62c5c6d48f64339560f403730e0341f4
COLLETTE Loïc <loic.collette@gmail.com>
2020-04-24 10:18:30 +0200

    delete .idea

--- .idea/fileTemplates/internal/JavaScript File.js
+++
@@ -1,8 +0,0 @@
-//-- ---------------------------------------------------- -->
-//--Auteurs : COLLETTE Loic et DELAVAL Kevin              -->
-//--Groupe : 2203                                         -->
-//--Labo : Programmation Web avancée                      -->
-//--Application : Site d'immersion à l'école              -->
-//--Date de la dernière mise à jour : [DATE DU JOUR]      -->
-//-- ---------------------------------------------------- -->
-
