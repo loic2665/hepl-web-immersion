59aeced488793aed0abd60da0fbd156438611c30
COLLETTE Loïc <loic.collette@gmail.com>
2020-04-24 11:53:21 +0200

    Auto stash before merge of "master" and "origin/master"

--- public/index.php
+++ public/index.php
@@ -26,9 +26,11 @@
 
               <div class="welcome-block">
 
-                  <div class="center while-text">
+                  <div class="center white-text">
 
-                        <h2>Bienvenue !</h2>
+                        <h1>Bienvenue !</h1>
+                      <p>Bienvenue sur HEPL immersion ! LE site vous permettant
+                      de vous inscrire aux journée découverte de la HEPL.</p>
 
                   </div>
 
@@ -38,9 +40,29 @@
           </div>
           <div class="col-xl-6">
 
-
               <div class="status-block">
 
+                  <div class="center white-text">
+                      <?php if(false){ ?>
+
+                          <h1>Les inscriptions sont actuellement ouvertes !</h1>
+                          <p>
+                              Les inscriptions sont ouvertes, depêchez-vous avant que toutes
+                              les places soient prises.
+                          </p>
+                          <p><a class="btn btn-primary">Voir les cours</a></p>
+
+                      <?php }else{ ?>
+
+                          <h1>Les inscriptions ne sont pas ouvertes...</h1>
+                          <p>
+                            Recevez un mail quand les inscriptions seront ouvertes
+                          </p>
+                          <p><a class="btn btn-warning">M'inscrire</a></p>
+
+                      <?php } ?>
+
+                  </div>
               </div>
 
           </div>
@@ -51,6 +73,15 @@
 
               <div class="nb-cours-block">
 
+                  <div class="center white-text">
+
+                      <h1>Nombre de cours disponibles</h1>
+                      <p>
+                          <?php try { echo(Cours::countAllCourses()); }catch (Error $e){ echo($e->getMessage()); } ?>
+                      </p>
+
+                  </div>
+
               </div>
 
           </div>
@@ -59,6 +90,15 @@
 
               <div class="nb-profs-block">
 
+                  <div class="center white-text">
+
+                      <h1>Nombre de cours disponibles</h1>
+                      <p>
+                          <?php try { echo(Enseignant::countAllTeachers()); }catch (Error $e){ echo($e->getMessage()); } ?>
+                      </p>
+
+                  </div>
+
               </div>
 
           </div>
@@ -66,6 +106,15 @@
 
 
               <div class="nb-days-block">
+
+                  <div class="center white-text">
+
+                      <h1>Cours disponibles</h1>
+                      <p>
+                          <?php try { echo(Cours::HowMuchDayRemaining()); }catch (Error $e){ echo($e->getMessage()); } ?>
+                      </p>
+
+                  </div>
 
               </div>
 
@@ -87,3 +136,4 @@
 
 </body>
 </html>
+
